a43eec56042f244537819a4da0d0794896149597
samynaj <samuelnnj@gmail.com>
2020-11-26 18:28:48 +0100

    added get and post request for todo list

--- app.js
+++ app.js
@@ -4,6 +4,33 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const items = [];
+
+app.set('view engine', "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    const today = new Date();
+    
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+    
+    var day = today.toLocaleDateString("en-US", options)
+    
+
+
+    res.render('list', {day: day, newListItems: items})
+})
+
+app.post('/', (req, res) => {
+    const newItem = req.body.newItem;
+    items.push(newItem);
+    console.log(newItem);
+
+    res.redirect('/');
+})
 
 app.listen(process.env.PORT || 3000, () => console.log('Server running on port 3000'));
